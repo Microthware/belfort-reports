366348517606d39e1e6cf0fcfdab9d8162286f36
microthware <randomrothstein@gmail.com>
2025-10-20 15:41:49 -0400

    cleanup

--- stock.py
+++ stock.py
@@ -3,22 +3,14 @@ import pandas as pd
 import argparse
 from bs4 import BeautifulSoup as bs
 from datetime import date
-from update_sheet import save_stock_list, update_stocks
 
 # Static vars
 key_list = ["4YHOULHQXIWM4ZEF", "6T0HXR5I4C22KLPO", "SPCWTGTBURVIVXWH", 
             "T0P44NA2G1AO3OGV", "D87A7X8OMGCXN7LF", "4A37TTIK2C01P5Q2", 
             "CRVTZ2LJCJEF94TV", "JDXUWTHM6V1QM4A0"]
 av_key = 0  # Needed to use alphavantage, get it from https://www.alphavantage.co/support/#api-key
-
-# Will's Gap-up filters
 gap_ups = "https://finviz.com/screener.ashx?v=111&f=sh_avgvol_o500,sh_relvol_o5,ta_changeopen_u5,ta_highlow20d_nh,ta_perf_d10o,ta_sma200_pa&ft=4&o=-changeopen"
-
-# PIVOT
 pivots = "https://finviz.com/screener.ashx?v=111&f=sh_avgvol_o500,sh_relvol_o1.5,ta_changeopen_u5,ta_highlow20d_nh,ta_sma200_pa&ft=4&ta=0&o=-changeopen"
-
-# TODO: Update stocks after follow-up day, and after 5 business days
-
 
 def stock_scanner():
     print("Starting...")
@@ -44,7 +36,6 @@ def stock_scanner():
         stock_list.extend([Get_Stock_Price(ticker)])
         # stock_list.extend([69, -1,-1,-1,-1])
         stocks_list.append(stock_list)
-    save_stock_list("gap_ups", stocks_list)
 
     stocks_list = []
     for ticker in pivot_stocks:
@@ -53,7 +44,6 @@ def stock_scanner():
         # stock_list.extend([69, -1,-1,-1,-1])
         if float(stock_list[-1]) > 2:
             stocks_list.append(stock_list)
-    save_stock_list("pivots", stocks_list)
 
 
 # returns the lisst of stock tickers from the finviz screener url
@@ -94,11 +84,6 @@ def Get_Intraday_Data(ticker, interval):
         print(yeet, " ", data[col_name][yeet])
 
 
-def stock_update():
-    update_stocks("gap_ups")
-    update_stocks("pivots")
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="command-line arguments")
     parser.add_argument("--scan", action="store_true", help="Scans for new pivot and gap up stocks")
@@ -107,9 +92,5 @@ if __name__ == "__main__":
 
     if args.scan is False and args.update is False:
         stock_scanner()
-        stock_update()
     elif args.scan:
         stock_scanner()
-    elif args.update:
-        stock_update()
-
